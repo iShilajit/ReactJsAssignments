8d8fc511f95f481ae1d02341974ca86ad1a6694b
Shilajit <shilajit2014paul@gmail.com>
2022-04-09 00:16:47 +0530

    add components
    add components

--- reactmenu/src/index.js
+++ reactmenu/src/index.js
@@ -3,10 +3,12 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import Menu from './menu/menu'
+import {component} from 'react-dom/client';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Menu />
   </React.StrictMode>,
   document.getElementById('root')
 );
