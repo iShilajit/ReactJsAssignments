d8e0ebfcdbbbf1ab05e1bc49f94acf71c329bfa3
Shilajit <shilajit2014paul@gmail.com>
2022-04-09 00:05:17 +0530

    menu
    menu

---
+++ reactmenu/src/menu/menu.jsx
@@ -0,0 +1,27 @@
+import './menu.css';
+
+const Menu=()=>{
+    return(
+        < div id="menu" >
+
+        <h1>Mobile Operating System</h1>
+        <ul>
+            <li>Android</li>
+            <li>BlackBerry</li>
+            <li>iphone</li>
+            <li>Windows Phoen</li>
+        </ul>
+
+
+        <h1>Mobile Manufraturers</h1>
+        <ul>
+            <li>Samsung</li>
+            <li>HTC</li>
+            <li>Micromax</li>
+            <li>Apple</li>
+        </ul>
+        </div>
+    );
+}
+
+export default Menu;
